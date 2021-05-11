526def3760fc45b263c1cfc1788c5cbeadcb1c91
ttjpffs <contacto@antoniojai.me>
2021-05-11 01:03:49 -0500

    Add track model, migrations and seeder

---
+++ database/factories/TrackFactory.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Track;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TrackFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Track::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $fakeGenres = [
+            'Art Electro Prog',
+            'Electro Norteño',
+            'Gangsta EDM',
+            'Gazecore',
+            'Hardcore',
+            'Post-Lofi',
+            'Space Latin',
+            'Shoegaze',
+            'Woke Twerk'
+        ];
+
+        return [
+            'title' => $this->faker->realText($maxNbChars=50),
+            'artist' => $this->faker->firstName() . ' ' . $this->faker->lastName(),
+            'genre' => $fakeGenres[array_rand($fakeGenres)],
+            'duration' => $this->faker->numberBetween(60, 600)
+        ];
+    }
+}
---
+++ database/migrations/2021_05_11_052342_create_tracks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTracksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tracks', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title');
+            $table->string('artist');
+            $table->unsignedInteger('duration'); // seconds
+            $table->string('genre');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tracks');
+    }
+}
