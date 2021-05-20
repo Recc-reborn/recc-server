12ba7ad0e79ed4c70854781aee3e87f428c0912d
tonyjaimep <tonyjaimep@gmail.com>
2021-05-19 19:17:30 -0500

    Add track routes and controller methods

--- app/Http/Controllers/TrackController.php
+++ app/Http/Controllers/TrackController.php
@@ -4,7 +4,43 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Track;
+
 class TrackController extends Controller
 {
-    //
+    public function index()
+    {
+        return Track::all();
+    }
+
+    public function show($id)
+    {
+        return Track::findOrFail($id);
+    }
+
+    public function store(Request $request)
+    {
+        return Track::create([
+            'title' => $request->title,
+            'artist' => $request->artist,
+            'duration' => $request->duration,
+            'genre' => $request->genre
+        ]);
+    }
+
+    public function update($id, Request $request)
+    {
+        $track = Track::findOrFail($id);
+        $track->title = $request->title;
+        $track->artist = $request->artist;
+        $track->duration = $request->duration;
+        $track->genre = $request->genre;
+
+        $track->save();
+    }
+
+    public function destroy($id)
+    {
+        Track::findOrFail($id)->delete();
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\TrackController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('tracks')->group(function () {
+    Route::get('/', [TrackController::class, 'index']);
+    Route::get('/{track:id}', [TrackController::class, 'show']);
+    Route::post('/', [TrackController::class, 'store']);
+    Route::put('/{track:id}', [TrackController::class, 'update']);
+    Route::delete('/{track:id}', [TrackController::class, 'destroy']);
 });
