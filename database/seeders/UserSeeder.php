bf93d4bf592d1725111eb6eba377216489a5a80d
Esteban Juarez <58830440+ElStevie@users.noreply.github.com>
2021-08-15 05:06:27 -0500

    Create UserSeeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::factory()
+            ->isAdmin()
+            ->create();
+
+        User::factory()
+            ->times(68)
+            ->create();
+    }
+}
