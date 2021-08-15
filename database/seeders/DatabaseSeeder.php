8475a38abb5979a43983cab244f034d1cbbeebdc
Esteban Juarez <58830440+ElStevie@users.noreply.github.com>
2021-08-15 05:10:19 -0500

    Update DatabaseSeeder class to use User and Playback seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(TracksTableSeeder::class);
+        $this->call([
+            UserSeeder::class,
+            TracksTableSeeder::class,
+            PlaybackSeeder::class,
+        ]);
     }
 }
