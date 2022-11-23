a0741faac06476ec12612ab3608b30786e8e1d7f
tonyjaimep <tonyjaimep@gmail.com>
2022-11-22 22:17:50 -0600

    Updates default controller logic

--- app/Http/Controllers/RecommendationController.php
+++ app/Http/Controllers/RecommendationController.php
@@ -4,10 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Playlist;
+
 class RecommendationController extends Controller
 {
     public function index()
     {
-        return [];
+        // TODO implement different recommendation strategies,
+        // and use this as a fallback
+        return Playlist::get();
     }
 }
