023b8e2899869316d0b2a28c75c85d53a451b28c
Manuel Sanchez <62370718+JoMaDotio@users.noreply.github.com>
2023-01-21 21:54:11 -0600

    The feature for preferred songs done (#22)
    * The feature for preferred songs done
    
    * gitignore for fuse files
    
    * Adding param definitions
    
    Co-authored-by: roblescristian4722 <roblescristian4722@gmail.com>

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Laravel\Sanctum\HasApiTokens;
 
@@ -36,6 +37,7 @@ class User extends Authenticatable
         'password',
         'remember_token',
         'set_preferred_artists_at',
+        'set_preferred_tracks_at'
     ];
 
     /**
@@ -53,22 +55,31 @@ class User extends Authenticatable
      * @var array
      */
     protected $appends = [
-        'has_set_preferred_artists'
+        'has_set_preferred_artists',
+        'has_set_preferred_tracks'
     ];
 
-    public function preferredArtists() : BelongsToMany
+    public function preferredArtists(): BelongsToMany
     {
         return $this->belongsToMany(Artist::class, 'preferred_artists', 'user_id', 'artist_id');
     }
 
-    public function addPreferredArtists(Array $artistIds)
+
+    /*
+     * @param $artistIds array
+     */
+    public function addPreferredArtists(array $artistIds): void
     {
         $this->preferredArtists()->syncWithoutDetaching($artistIds);
         $this->set_preferred_artists_at = now();
         $this->save();
     }
 
-    public function removePreferredArtists(Array $artistIds)
+
+    /*
+     * @param $artistIds array
+     */
+    public function removePreferredArtists(array $artistIds): void
     {
         $this->preferredArtists()->detach($artistIds);
     }
@@ -77,6 +88,37 @@ class User extends Authenticatable
     {
         return Attribute::make(
             get: fn () => !empty($this->set_preferred_artists_at)
+        );
+    }
+
+    public function preferredTracks(): BelongsToMany
+    {
+        return $this->belongsToMany(Track::class, 'preferred_tracks', 'user_id', 'track_id');
+    }
+
+
+    /*
+     * @param $tracksIds array
+     */
+    public function addPreferredTracks(array $tracksIds): void
+    {
+        $this->preferredTracks()->syncWithoutDetaching($tracksIds);
+        $this->set_preferred_tracks_at = now();
+        $this->save();
+    }
+
+    /*
+     * @param $tracksIds array
+     */
+    public function removePreferredTracks(array $tracksIds): void
+    {
+        $this->preferredTracks()->detach($tracksIds);
+    }
+
+    public function hasSetPreferredTracks(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => !empty($this->set_preferred_tracks_at)
         );
     }
 }
