b62b6f9ae3935a3d69b8a81819d373d9d699dec9
roblescristian4722 <roblescristian4722@gmail.com>
2023-01-23 04:30:57 -0600

    removing duplicated artists

--- app/Console/Commands/CloneLastFMArtists.php
+++ app/Console/Commands/CloneLastFMArtists.php
@@ -85,7 +85,15 @@ class CloneLastFMArtists extends Command
         $initWarn = str_replace(array("\r", "\n", "    ", "\t"), "", $initWarn);
         $this->warn($initWarn);
 
+
+        $this->info(">>> FETCHING ARTISTS");
+        // Artists
         $this->fetchArtists();
+
+
+        $this->info(">>> FETCHING TRACKS");
+        // Tracks
+        $this->fetchTracks();
 
         return 0;
     }
@@ -94,9 +102,10 @@ class CloneLastFMArtists extends Command
      * Fetches tracks from LastFM
      * @param mixed $pageArtists
      */
-    protected function fetchTracks($pageArtists): void
+    protected function fetchTracks(): void
     {
-        foreach ($pageArtists as $artist) {
+        $artists = Artist::all();
+        foreach ($artists as $artist) {
             $tracks = $this->getTrackPage($artist->name);
             if (!isset($tracks->toptracks->track)) {
                 $this->error("'$artist->name'\'s tracks couldn't get fetched");
@@ -141,11 +150,10 @@ class CloneLastFMArtists extends Command
         // whatever happens first: artist limit reached or an error response
         for ($page = 1; $artistFetched < $totalArtist; $page++) {
             $zeroFilledPageNumber = str_pad((string) $page, 2, "0", STR_PAD_LEFT);
-            $this->info("Retrieving page $zeroFilledPageNumber: $artistFetched/$totalArtist artists fetched");
+            $this->info(">>> Retrieving page $zeroFilledPageNumber: $artistFetched/$totalArtist artists fetched");
             try {
                 $result = $this->getArtistPage($page);
                 $pageArtists = $result->artists->artist;
-                $this->fetchTracks($pageArtists);
 
                 $this->addArtistsToDatabase($pageArtists);
 
@@ -155,13 +163,13 @@ class CloneLastFMArtists extends Command
                 if (!$pageArtists || $artistCount < $this->minArtistPerPage) {
                     // not enough artists to complete page, we'll take it as if
                     // we retrieved all retrievable artists
-                    $this->warn("Not enough artists ($artistCount) to fill a page ($this->artistsPerPage). Leaving seeder.");
+                    $this->warn(">>> Not enough artists ($artistCount) to fill a page ($this->artistsPerPage). Leaving seeder.");
                 }
             } catch (Exception $e) {
                 // error response
                 if ($totalRetries < $this->retryLimit) {
                     $this->warn($e->getMessage());
-                    $this->warn("Error retrieving top artists. Trying again ($totalRetries / $this->retryLimit).");
+                    $this->warn(">>> Error retrieving top artists. Trying again ($totalRetries / $this->retryLimit).");
                     // try getting this page again
                     $totalRetries++;
                     $page--;
@@ -234,8 +242,10 @@ class CloneLastFMArtists extends Command
         // LastFM responses are weird
         foreach ($artists as $artist) {
             if (Artist::where('last_fm_url', $artist->url)->exists()) {
+                $this->warn("Not adding duplicated artist \"$artist->name\"");
                 continue;
             }
+            $this->info("Adding artist \"$artist->name\" to database");
 
             Artist::create([
                 'name' => $artist->name,
@@ -261,14 +271,11 @@ class CloneLastFMArtists extends Command
                 'name' => trim(strtolower($tag->name)),
                 'url' => $tag->url
             ]);
-            if ($tag->name == "brazilian") {
-                $this->error("Look at me, i'm been saved: $tag->name - Id: $tag->id");
-            }
             return $tag->id;
         }
         $id = Tag::where('name', trim(strtolower($tag->name)))->value('id');
         if (!isset($id)) {
-            $this->error("Unexpected error finding the tag: $tag->name - Id: $id");
+            $this->error(">>> Unexpected error finding the tag: $tag->name - Id: $id");
             return -1;
         }
         return $id;
@@ -288,6 +295,7 @@ class CloneLastFMArtists extends Command
                     $this->error("$artist - $track->name couldn't get added to database");
                     continue;
                 }
+                $this->info("$artist - $track->name added to database");
                 $tags = $this->getTagIds($trackInfo);
 
                 $trackInfo = $trackInfo->track;
@@ -311,8 +319,8 @@ class CloneLastFMArtists extends Command
 
                 $track->tags()->sync($tags);
             } catch (Exception $e) {
-                $this->error("Not enough tracks fetched: $e");
-                return;
+                $this->error("$artist - $track->name couldn't get added to database");
+                continue;
             }
         }
     }
