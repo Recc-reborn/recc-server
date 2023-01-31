ee3de6e9368fa93b8b221cb3222dd959f109ca99
JoMaDotio <eltaquero@hotmail.com>
2023-01-30 22:14:54 -0600

    Adding the funtionality for the create playlist endpoint, for now is mocked

--- app.py
+++ app.py
@@ -1,10 +1,39 @@
-from flask import Flask
+from werkzeug.exceptions import HTTPException
+from flask import Flask, jsonify, request
 from dotenv import load_dotenv
+import json
+import random
 
 load_dotenv()
 
 app = Flask(__name__)
 
+
+@app.errorhandler(HTTPException)
+def handle_exception(e):
+    """Return JSON instead of HTML for HTTP errors."""
+    # start with the correct headers and status code from the error
+    response = e.get_response()
+    # replace the body with JSON
+    response.data = json.dumps({
+        "code": e.code,
+        "name": e.name,
+        "description": e.description,
+    })
+    response.content_type = "application/json"
+    return response
+
+
 @app.route("/")
 def greet():
     return "Hello :)"
+
+
+@app.route("/create_playlist", methods=["GET"])
+def create_playlist():
+    if request.method != "GET":
+        app.logger.warning("Unsoported method call")
+        raise HTTPException("Unsoported request method")
+
+    # For now return indexes between 0 and 1000, we can used as they are the songs id
+    return jsonify([random.randint(0, 1000) for _ in range(10)])
