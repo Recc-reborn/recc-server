b31f2245237e034c4776b3b9381f3040a89d796f
roblescristian4722 <roblescristian4722@gmail.com>
2023-02-19 21:45:47 -0600

    Using Auth:user instead of parameters

--- app/Http/Controllers/PlaybackController.php
+++ app/Http/Controllers/PlaybackController.php
@@ -3,16 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use App\Models\Playback;
 
 class PlaybackController extends Controller
 {
     public function store(Request $request)
     {
-        return Playback::create([
-            'track_id' => $request->track_id,
-            'user_id' => $request->user()->id
-        ]);
+        if (Auth::check()) {
+            $user = Auth::user();
+            return Playback::create([
+                'track_id' => $request->track_id,
+                'user_id' => $user->id
+            ]);
+        }
+        return response("Unauthenticated", 401);
     }
 
     public function destroy(Playback $playback)
