10cf20ca6faa49640b728f3e8f7c47df81c5f41c
JoMaDotio <eltaquero@hotmail.com>
2023-03-18 15:16:28 -0600

    Saving changes for now

--- app.py
+++ app.py
@@ -4,8 +4,12 @@ from dotenv import load_dotenv
 import json
 import random
 
+from db.create_db_engine import create_db_engine
+from db.querys_db import query_all_tracks
+
 load_dotenv()
 
+connection = create_db_engine()
 app = Flask(__name__)
 
 
@@ -40,3 +44,14 @@ def create_playlist():
     }
     # For now return indexes between 0 and 1000, we can used as they are the songs id
     return jsonify(response)
+
+
+@app.route("/test", methods=['GET'])
+def demo():
+    if request.method != "GET":
+        app.logger.warning("Unsoported method call")
+        raise HTTPException("Unsoported request method")
+
+    response = query_all_tracks(connection)
+
+    return jsonify([dict(dict_row) for dict_row in response.mappings()])
