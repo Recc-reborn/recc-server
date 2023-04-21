b458e28430f0d1099c25cf35f2753c392ae8eedc
roblescristian4722 <roblescristian4722@gmail.com>
2023-04-21 17:31:03 -0600

    werkzeug added

--- init.sh
+++ init.sh
@@ -2,10 +2,10 @@
 
 cd /app
 
-pip3 install pipenv pandas colors.py rfc3339 numpy
+pip3 install pipenv pandas colors.py rfc3339 numpy werkzeug
 
 pipenv install --python /usr/bin/python3
 
-pipenv run pip install pandas colors.py rfc3339 numpy
+pipenv run pip install pandas colors.py rfc3339 numpy werkzeug
 
 make start-prod
